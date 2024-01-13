0a3f691584abeed6e13f8ca36c58a7b08e4a6446
RomelTibay <154564461+RomelTibay@users.noreply.github.com>
2024-01-13 01:42:41 +0000

    Done to 1.5

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -6,26 +6,63 @@
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
 // Your code here
 
+var x;
+x = 5
+
+console.log(x)
+
+
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
+
+let mySpeech
+mySpeech = "Hello"
+console.log(mySpeech)
 
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
 
+//const con = 1.1
+//con = 50
+//console.log(con)
+
+
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer: A const variable cannot be reassigned, variables defined with 'let' can not be redeclared and var create variables that can be reassigned another value
 
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
 
+var x;
+x = 5
+var y;
+y = 6
+
+var z;
+z = x + y;
+
+var q = 10
+var r = 5
+
+var u = q / r
+
+console.log(z)
+console.log(u)
+
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
+// Answer:I used the + and / operators
 // Your code here
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
 // Your code here
+
+let firstName = "Romel"
+let lastName = "Tibay"
+let fullName = firstName +" "+ lastName
+
+console.log(fullName)
 
 // Checkpoint 1.3 What operators did you use?
 // Answer: 
